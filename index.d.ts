06d9157e1aa62c5846a5b4989df7484edd49475e
Petr Tripolsky <tripolskypetr@gmail.com>
2020-11-07 11:02:42 +0300

    typing

---
+++ index.d.ts
@@ -0,0 +1,8 @@
+declare module "pahom" {
+
+  /**
+   * @description Запускает таймер на вывод цитаты в ближайшие 7 часов 7 минут 7 секунд
+   */
+  export function makeUff(...args: any): any;
+
+}
--- index.mjs
+++ index.mjs
@@ -1,6 +1,9 @@
 import closestTime from './src/closestTime.mjs';
 import epiphany from './src/epiphany.mjs';
 
+/**
+ * @description Запускает таймер на вывод цитаты в ближайшие 7 часов 7 минут 7 секунд
+ */
 export const makeUff = () => setTimeout(epiphany, closestTime());
 
 export default { makeUff };
